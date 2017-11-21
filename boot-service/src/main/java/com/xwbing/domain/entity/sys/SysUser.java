ee30db5e5c14fee69f5d1094767088cd15a689bb
xiangwb <xiangwb@drore.com>
2017-11-21 15:49:34 +0800

    merge

--- boot-service/src/main/java/com/xwbing/domain/entity/sys/SysUser.java
+++ boot-service/src/main/java/com/xwbing/domain/entity/sys/SysUser.java
@@ -26,30 +26,22 @@ import java.util.List;
 public class SysUser extends BaseEntity {
     private static final long serialVersionUID = -2447528751353457021L;
     public static String table = "sys_user_info";
-    /**
-     * 用户名
-     */
     @NotBlank(message = "用户名不能为空")
     @Length(min = 1, max = 20, message = "用户名长度为1-20")
     @Column(name = "user_name")
+    @ApiModelProperty(value = "用户名",example = "xxx")
     private String userName;
-    /**
-     * 姓名
-     */
     @NotBlank(message = "姓名不能为空")
     @Length(min = 1, max = 20, message = "姓名长度为1-5")
+    @ApiModelProperty(value = "姓名",example = "xxx")
     private String name;
-    /**
-     * 邮箱
-     */
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式有误")
+    @ApiModelProperty(value = "邮箱",example = "@163.com")
     private String mail;
-    /**
-     * 性别
-     */
     @NotBlank(message = "性别不能为空")
     @Pattern(regexp = "[01]", message = "性别格式为0|1,0代表女,1代表男")
+    @ApiModelProperty(value = "性别",example = "1")
     private String sex;
     @ApiModelProperty(value = "盐值",hidden = true)
     private String salt;
