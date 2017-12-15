56fecc25c88d166b7e69bd0ab25c6a333de06cbf
xiangwb <xiangwb@drore.com>
2017-12-15 16:37:33 +0800

    sql demo

--- boot-service/src/main/java/com/xwbing/service/DemoService.java
+++ boot-service/src/main/java/com/xwbing/service/DemoService.java
@@ -1,34 +1,4 @@
 package com.xwbing.service;
 
-import com.xwbing.util.RestMessage;
-import org.apache.commons.lang3.StringUtils;
-
 public class DemoService {
-    void setRemind(String id, String typeName, String remindDate, String type) {
-        RestMessage one = queryOne(id, typeName);
-        if (one != null) {
-            if (StringUtils.isNotEmpty(remindDate)) {
-                update();
-            } else {
-                delete();
-            }
-        } else {
-            if (StringUtils.isNotEmpty(remindDate)) {
-                save();
-            }
-        }
-    }
-
-    private RestMessage queryOne(String id, String typeName) {
-        return new RestMessage();
-    }
-
-    private void update() {
-    }
-
-    private void delete() {
-    }
-
-    private void save() {
-    }
 }
