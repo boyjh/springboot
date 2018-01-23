ee9eef53968050cf2d3d5334435f3a7e9ac752d1
xiangwb <xiangwb@drore.com>
2018-01-23 14:45:16 +0800

    merge

---
+++ boot-service/src/main/java/com/xwbing/demo/demo.java
@@ -0,0 +1,13 @@
+package com.xwbing.demo;
+
+/**
+ * 项目名称: boot-module-demo
+ * 创建时间: 2018/1/23 14:45
+ * 作者: xiangwb
+ * 说明:
+ */
+public class demo {
+    public static void main(String[] args) {
+
+    }
+}
