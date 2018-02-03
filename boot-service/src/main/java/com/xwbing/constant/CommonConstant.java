88809493c1c0d3203776cd0d83466ed4fa8f26f2
xiangwb <xiangwb@drore.com>
2018-02-03 16:50:16 +0800

    merge

--- boot-service/src/main/java/com/xwbing/constant/CommonConstant.java
+++ boot-service/src/main/java/com/xwbing/constant/CommonConstant.java
@@ -48,4 +48,12 @@ public class CommonConstant {
      * 导出报表列名称
      */
     public static final String[] USER_REPORT_COLUMNS = new String[]{" 名字", "用户名", "性别", "邮箱", "是否为管理员"};
+    /**
+     * 中国各省市
+     */
+    public static final String CHINA = "北京,天津,河北,山西,内蒙古,辽宁,吉林,黑龙江,上海,江苏,安徽,福建,江西,山东,河南,湖北,湖南,广东,广西,海南,重庆,四川,贵州,云南,西藏,陕西,甘肃,青海,宁夏,新疆,台湾,香港,澳门";
+    /**
+     * 浙江各市
+     */
+    public static final String ZHEJIANG = "杭州,宁波,温州,绍兴,湖州,嘉兴,金华,衢州,台州,丽水,舟山";
 }
