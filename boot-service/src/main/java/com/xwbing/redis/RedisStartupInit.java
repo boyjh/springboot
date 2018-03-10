4fad5dc3d9bd8faa5cdee7565c40c0da09e58952
xiangwbs <xiangwbs@163.com>
2018-03-10 18:29:42 +0800

    merge

--- boot-service/src/main/java/com/xwbing/redis/RedisStartupInit.java
+++ boot-service/src/main/java/com/xwbing/redis/RedisStartupInit.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -30,16 +29,15 @@ public class RedisStartupInit implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         logger.info("redis has been start");
         System.out.println("启动执行方法======刷新缓存==================");
-        redisService.set("xwb", "测试数据");
-        System.out.printf(redisService.get("xwb"));
+        redisService.set("test", "插入数据成功");
+        logger.info("redis获取测试数据:{}",redisService.get("test"));
         redisService.del("xwb");
-        System.out.printf(String.valueOf(redisService.exists("xwb")));
+        boolean exists = redisService.exists("xwb");
+        logger.info("redis测试成功:{}",!exists);
         Set<String> set = redisService.keys(redisCode + "*");
-        Iterator<String> it = set.iterator();
-        while (it.hasNext()) {
-            String keyStr = it.next();
-            System.out.println("启动删除缓存名称==" + keyStr);
+        set.forEach(keyStr -> {
+            logger.info("删除缓存名称=={}",keyStr);
             redisService.delInit(keyStr);
-        }
+        });
     }
 }
