ee77530a3adea5035adb7ffa04f7d3481281588b
xiangwbs <xiangwbs@163.com>
2018-03-10 14:04:04 +0800

    merge

--- boot-service/src/main/java/com/xwbing/demo/Ademo.java
+++ boot-service/src/main/java/com/xwbing/demo/Ademo.java
@@ -2,7 +2,6 @@ package com.xwbing.demo;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 项目名称: boot-module-demo
@@ -13,19 +12,32 @@ import java.util.stream.Collectors;
 public class Ademo {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             integers.add(i);
         }
-        long l = System.currentTimeMillis();
-        List<Integer> collect = integers.stream().filter(integer ->integer==2 ).collect(Collectors.toList());
-        long l1 = System.currentTimeMillis();
-        System.out.println(l1-l);
-        System.out.println(collect);
+//        long l = System.currentTimeMillis();
+//        integers.forEach(integer -> {
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        long l1 = System.currentTimeMillis();
+//        System.out.println(l1-l);
+
         long l3 = System.currentTimeMillis();
-        List<Integer> collect1 = integers.parallelStream().filter(integer ->integer==2 ).collect(Collectors.toList());
+        integers.parallelStream().forEach(integer -> {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         long l4 = System.currentTimeMillis();
         System.out.println(l4-l3);
-        System.out.println(collect1);
+
+
 
     }
 }
