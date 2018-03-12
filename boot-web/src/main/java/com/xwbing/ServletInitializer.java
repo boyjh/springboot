4dcf0e01d33d01c39f5dde0db5f62f5a6eea47cb
xiangwbs <xiangwbs@163.com>
2018-03-12 16:18:01 +0800

    merge

--- boot-web/src/main/java/com/xwbing/ServletInitializer.java
+++ boot-web/src/main/java/com/xwbing/ServletInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
     /**
-     * 注册启动类Application。外部web应用服务器构建ApplicationContext的时候，会把启动类添加进去。
+     * 注册启动类。外部web应用服务器构建ApplicationContext的时候，会把启动类添加进去。
      *
      * @param application
      * @return
