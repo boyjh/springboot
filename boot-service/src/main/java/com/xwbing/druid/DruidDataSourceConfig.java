bf1ca2db28bf54ffa25bb76ce11a10bab9002af9
xiangwb <xiangwb@drore.com>
2018-05-09 10:22:15 +0800

    merge

--- boot-service/src/main/java/com/xwbing/druid/DruidDataSourceConfig.java
+++ boot-service/src/main/java/com/xwbing/druid/DruidDataSourceConfig.java
@@ -1,9 +1,13 @@
 package com.xwbing.druid;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,12 +22,11 @@ import java.sql.SQLException;
  * 作者:  xiangwb
  */
 @Configuration
-//@ConfigurationProperties(prefix = "spring.datasource")
 @EnableConfigurationProperties(DruidDataSourceModel.class)
 public class DruidDataSourceConfig {
     @Resource
     private DruidDataSourceModel druidDataSourceModel;
-    private final Logger logger= LoggerFactory.getLogger(DruidDataSourceConfig.class);
+    private final Logger logger = LoggerFactory.getLogger(DruidDataSourceConfig.class);
 
     @Bean
     @Primary
@@ -53,5 +56,31 @@ public class DruidDataSourceConfig {
             logger.error("配置druid过滤器异常");
         }
         return dataSource;
+    }
+
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        logger.info("注册druid监控信息显示statViewServlet ======================= ");
+        ServletRegistrationBean registration = new ServletRegistrationBean(new StatViewServlet());
+        registration.addUrlMappings("/druid/*");
+        //添加初始化参数:initParams
+//        registration.addInitParameter("allow", "127.0.0.1");//IP白名单(没有配置或者为空,则允许所有访问)
+//        registration.addInitParameter("deny", "172.17.21.232");//IP黑名单(存在共同时,deny优先于allow)
+        registration.addInitParameter("loginUsername", "admin");//用户名
+        registration.addInitParameter("loginPassword", "123456");//密码
+        registration.addInitParameter("resetEnable", "true");//禁用HTML页面上的"Reset All"功能
+        return registration;
+    }
+
+    /*servlet*****************************************************filter*/
+
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+        logger.info("注册druid监控信息采集webStatFilter ======================= ");
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.jsp,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");//监控单个url调用的sql列表
+        return filterRegistrationBean;
     }
 }
--- boot-web/src/main/java/com/xwbing/configuration/ServletFilterConfig.java
+++ boot-web/src/main/java/com/xwbing/configuration/ServletFilterConfig.java
@@ -1,7 +1,5 @@
 package com.xwbing.configuration;
 
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
 import com.xwbing.handler.FormRepeatFilter;
 import com.xwbing.util.captcha.CaptchaServlet;
 import org.slf4j.Logger;
@@ -29,31 +27,7 @@ public class ServletFilterConfig {
         return registration;
     }
 
-    @Bean
-    public ServletRegistrationBean statViewServlet() {
-        logger.info("注册druid监控信息显示statViewServlet ======================= ");
-        ServletRegistrationBean registration = new ServletRegistrationBean(new StatViewServlet());
-        registration.addUrlMappings("/druid/*");
-        //添加初始化参数:initParams
-//        registration.addInitParameter("allow", "127.0.0.1");//IP白名单(没有配置或者为空,则允许所有访问)
-//        registration.addInitParameter("deny", "172.17.21.232");//IP黑名单(存在共同时,deny优先于allow)
-        registration.addInitParameter("loginUsername", "admin");//用户名
-        registration.addInitParameter("loginPassword", "123456");//密码
-        registration.addInitParameter("resetEnable", "true");//禁用HTML页面上的"Reset All"功能
-        return registration;
-    }
-
     /*servlet*****************************************************filter*/
-
-    @Bean
-    public FilterRegistrationBean webStatFilter() {
-        logger.info("注册druid监控信息采集webStatFilter ======================= ");
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.jsp,/druid/*");
-        filterRegistrationBean.addInitParameter("profileEnable", "true");//监控单个url调用的sql列表
-        return filterRegistrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean formSaveFilter() {
