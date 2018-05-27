0a7b009300ed6aa5cad59e4ac6670fc5498a8e2c
xiangwbs <xiangwbs@163.com>
2018-05-27 19:07:17 +0800

    添加mysql命令

--- boot-service/src/main/java/com/xwbing/demo/sql/sql.sql
+++ boot-service/src/main/java/com/xwbing/demo/sql/sql.sql
@@ -3,6 +3,16 @@ show variables like 'slow_query%';
 查询超过多少秒才记录
 show variables like 'long_query_time';
 
+mysqld --remove 卸载mysql服务
+sc delete mysql 彻底删除mysql服务
+mysqld --install [mysql] 安装mysql服务，默认名为mysql
+初始化data目录
+mysqld --initialize-insecure [--console] 没有密码
+mysqld --initialize [--console] 有密码，密码在data/err文件里 console密码打印控制台
+net start mysql
+net stop mysql
+
+
 mysql -v
 连接mysql
 mysql -u用户名 -p密码
