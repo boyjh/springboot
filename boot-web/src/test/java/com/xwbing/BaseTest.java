9b83052bd3afd012ba8a51c08a6e939fcaebc632
xiangwb <xiangwbs@163.com>
2018-07-21 19:22:05 +0800

    merge

--- boot-web/src/test/java/com/xwbing/BaseTest.java
+++ boot-web/src/test/java/com/xwbing/BaseTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-//@Rollback(false)
+@Rollback(false)
 @WebAppConfiguration
 public class BaseTest {
     @Autowired
