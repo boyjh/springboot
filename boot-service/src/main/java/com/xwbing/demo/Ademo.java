fb8f82fe3821720d6b6edfe5eb2314abecef3153
xiangwb <xiangwbs@163.com>
2018-10-22 22:27:40 +0800

    merge

--- boot-service/src/main/java/com/xwbing/demo/Ademo.java
+++ boot-service/src/main/java/com/xwbing/demo/Ademo.java
@@ -24,6 +24,8 @@ public class Ademo {
         for (int i = 0; i < 50; i++) {
             list.add(i);
         }
+
+
         List<Integer> reslut = new ArrayList<>();
         list.parallelStream().forEach(reslut::add);
         System.out.println(reslut.size());
