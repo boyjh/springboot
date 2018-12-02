6e277abd57c06f723d8ab06887a5e977817eaf46
xiangwb <xiangwbs@163.com>
2018-12-02 11:38:59 +0800

    线程通信demo

--- boot-service/src/main/java/com/xwbing/demo/ThreadCommunication.java
+++ boot-service/src/main/java/com/xwbing/demo/ThreadCommunication.java
@@ -13,7 +13,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author xiangwb
  * @date 2018/12/2 09:30
  * @description 线程间的通信
- * 编写两个线程，一个线程打印1~25，另一个线程打印字母A~Z，打印顺序为12A34B56C……5152Z
+ * 编写两个线程，一个线程打印1~52，另一个线程打印字母A~Z，打印顺序为12A34B56C……5152Z
  */
 public class ThreadCommunication {
     private static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(20, 35, 10,
