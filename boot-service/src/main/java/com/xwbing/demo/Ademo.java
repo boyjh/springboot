59d35eb9005584abda9304ad36c8810ebb434f11
xiangwb <xiangwbs@163.com>
2018-12-02 13:55:40 +0800

    ???可见性

--- boot-service/src/main/java/com/xwbing/demo/Ademo.java
+++ boot-service/src/main/java/com/xwbing/demo/Ademo.java
@@ -1,11 +1,6 @@
 package com.xwbing.demo;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import lombok.extern.java.Log;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 项目名称: boot-module-pro
@@ -15,21 +10,18 @@ import java.util.concurrent.TimeUnit;
  */
 @Log
 public class Ademo {
-    private Cache<String, List<String>> dyInfosCache = CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(24, TimeUnit.HOURS).recordStats().build();
+    public static void main(String[] args) throws InterruptedException {
+        MyThrea thread = new MyThrea();
+        thread.start();
+        Thread.sleep(5);
+        thread.setRunning(false);
+        System.out.println("main end " + thread.isRunning());
 
-    public static void main(String[] args) {
-        try {
-            MyThrea thread = new MyThrea();
-            thread.start();
-            Thread.sleep(1);
-            thread.setRunning(false);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
+
 class MyThrea extends Thread {
-    private boolean isRunning = true;
+    private  boolean isRunning = true;
 
     public boolean isRunning() {
         return isRunning;
@@ -42,8 +34,10 @@ class MyThrea extends Thread {
     @Override
     public void run() {
         System.out.println("进入到run方法中了");
-        while (isRunning == true) {
+        while (isRunning) {
         }
         System.out.println("线程执行完成了");
     }
+
+
 }
