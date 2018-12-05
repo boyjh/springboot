a65bacbe0beafa2e160f8b39e6506aad560da40f
xiangwb <xiangwbs@163.com>
2018-12-05 20:29:37 +0800

    Volatile使用demo 线程之间通信demo：共享变量和消息传递

--- boot-service/src/main/java/com/xwbing/demo/Ademo.java
+++ boot-service/src/main/java/com/xwbing/demo/Ademo.java
@@ -10,34 +10,9 @@ import lombok.extern.java.Log;
  */
 @Log
 public class Ademo {
+
     public static void main(String[] args) throws InterruptedException {
-        MyThrea thread = new MyThrea();
-        thread.start();
-        Thread.sleep(5);
-        thread.setRunning(false);
-        System.out.println("main end " + thread.isRunning());
 
     }
 }
 
-class MyThrea extends Thread {
-    private  boolean isRunning = true;
-
-    public boolean isRunning() {
-        return isRunning;
-    }
-
-    public void setRunning(boolean isRunning) {
-        this.isRunning = isRunning;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("进入到run方法中了");
-        while (isRunning) {
-        }
-        System.out.println("线程执行完成了");
-    }
-
-
-}
--- boot-service/src/main/java/com/xwbing/demo/ThreadCommunication.java
+++ boot-service/src/main/java/com/xwbing/demo/ThreadCommunication.java
@@ -1,9 +1,7 @@
 package com.xwbing.demo;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Queue;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -12,14 +10,18 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author xiangwb
  * @date 2018/12/2 09:30
- * @description 线程间的通信
+ * @description 线程间的通信：共享变量和消息传递
  * 编写两个线程，一个线程打印1~52，另一个线程打印字母A~Z，打印顺序为12A34B56C……5152Z
+ * 多线程特性三大概念：
+ * 原子性：jmm保证了对基本数据类型变量简单的读取和赋值(必须是将数字直接赋值给某个变量，变量之间相互赋值不是原子操作)是原子操作，如果要大范围操作的原子性，可以通过synchronized和lock
+ * 可见性：volatile。synchronized和lock能保证同一个时刻只有一个线程获取锁然后执行同步代码，并且在释放锁之前会将对变量的修改刷新到主内存
+ * 有序性：volatile。synchronized和lock保证每个时刻是有一个线程执行同步代码，相当于是让线程顺序执行同步代码
  */
 public class ThreadCommunication {
     private static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(20, 35, 10,
             TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000), new ThreadPoolExecutor.DiscardPolicy());
 
-    public static String[] buildNoArr(int max) {
+    static String[] buildNoArr(int max) {
         String[] noArr = new String[max];
         for (int i = 0; i < max; i++) {
             noArr[i] = Integer.toString(i + 1);
@@ -27,7 +29,7 @@ public class ThreadCommunication {
         return noArr;
     }
 
-    public static String[] buildCharArr(int max) {
+    static String[] buildCharArr(int max) {
         String[] charArr = new String[max];
         int tmp = 65;
         for (int i = 0; i < max; i++) {
@@ -36,7 +38,7 @@ public class ThreadCommunication {
         return charArr;
     }
 
-    public static void print(String... input) {
+    static void print(String... input) {
         if (input == null)
             return;
         for (String each : input) {
@@ -46,26 +48,35 @@ public class ThreadCommunication {
 
 
     public static void main(String[] args) throws InterruptedException {
-        MethodOne one = new MethodOne();
-        CompletableFuture.runAsync(one.threadOne(), EXECUTOR);
-        CompletableFuture.runAsync(one.threadTwo(), EXECUTOR);
-        Thread.sleep(10);
-        System.out.println("==synchronized、notify、wait==");
-        MethodTwo two = new MethodTwo();
-        CompletableFuture.runAsync(two.threadOne(), EXECUTOR);
-        CompletableFuture.runAsync(two.threadTwo(), EXECUTOR);
-        Thread.sleep(10);
-        System.out.println("==Lock、Condition==");
-        MethodFour four = new MethodFour();
-        CompletableFuture.runAsync(four.threadOne(), EXECUTOR);
-        CompletableFuture.runAsync(four.threadTwo(), EXECUTOR);
-        Thread.sleep(10);
-        System.out.println("==AtomicInteger==");
-        MethodThree three = new MethodThree();
-        CompletableFuture.runAsync(three.threadOne(), EXECUTOR);
-        CompletableFuture.runAsync(three.threadTwo(), EXECUTOR);
-        Thread.sleep(10);
-        System.out.println("==volatile==");
+        try {
+            MethodOne one = new MethodOne();
+            CompletableFuture.runAsync(one.threadOne(), EXECUTOR);
+            CompletableFuture.runAsync(one.threadTwo(), EXECUTOR);
+            Thread.sleep(10);
+            System.out.println("==synchronized、notify、wait==");
+            MethodTwo two = new MethodTwo();
+            CompletableFuture.runAsync(two.threadOne(), EXECUTOR);
+            CompletableFuture.runAsync(two.threadTwo(), EXECUTOR);
+            Thread.sleep(10);
+            System.out.println("==Lock、Condition==");
+            MethodThree three = new MethodThree();
+            CompletableFuture.runAsync(three.threadOne(), EXECUTOR);
+            CompletableFuture.runAsync(three.threadTwo(), EXECUTOR);
+            Thread.sleep(10);
+            System.out.println("==volatile==");
+            MethodFour four = new MethodFour();
+            CompletableFuture.runAsync(four.threadOne(), EXECUTOR);
+            CompletableFuture.runAsync(four.threadTwo(), EXECUTOR);
+            Thread.sleep(10);
+            System.out.println("==AtomicInteger==");
+            MethodFive five = new MethodFive();
+            CompletableFuture.runAsync(five.threadOne(), EXECUTOR);
+            CompletableFuture.runAsync(five.threadTwo(), EXECUTOR);
+            Thread.sleep(10);
+            System.out.println("==queue==");
+        } finally {
+            EXECUTOR.shutdown();
+        }
     }
 }
 
@@ -81,7 +92,7 @@ class MethodOne {
         int value = 1;
     }
 
-    public Runnable threadOne() {
+    Runnable threadOne() {
         final String[] inputArr = ThreadCommunication.buildNoArr(52);
         return new Runnable() {
             private String[] arr = inputArr;
@@ -98,30 +109,30 @@ class MethodOne {
                         }
                     }
                 } catch (InterruptedException e) {
-                    System.out.println("Oops...");
+                    e.printStackTrace();
                 }
             }
         };
     }
 
-    public Runnable threadTwo() {
+    Runnable threadTwo() {
         final String[] inputArr = ThreadCommunication.buildCharArr(26);
         return new Runnable() {
             private String[] arr = inputArr;
 
             public void run() {
                 try {
-                    for (int i = 0; i < arr.length; i++) {
+                    for (String anArr : arr) {
                         synchronized (share) {
                             while (share.value == 1)
                                 share.wait();
-                            ThreadCommunication.print(arr[i]);
+                            ThreadCommunication.print(anArr);
                             share.value = 1;
                             share.notify();
                         }
                     }
                 } catch (InterruptedException e) {
-                    System.out.println("Oops...");
+                    e.printStackTrace();
                 }
             }
         };
@@ -140,7 +151,7 @@ class MethodTwo {
         int value = 1;
     }
 
-    public Runnable threadOne() {
+    Runnable threadOne() {
         final String[] inputArr = ThreadCommunication.buildNoArr(52);
         return new Runnable() {
             private String[] arr = inputArr;
@@ -164,18 +175,18 @@ class MethodTwo {
         };
     }
 
-    public Runnable threadTwo() {
+    Runnable threadTwo() {
         final String[] inputArr = ThreadCommunication.buildCharArr(26);
         return new Runnable() {
             private String[] arr = inputArr;
 
             public void run() {
-                for (int i = 0; i < arr.length; i++) {
+                for (String anArr : arr) {
                     try {
                         lock.lock();
                         while (share.value == 1)
                             condition.await();
-                        ThreadCommunication.print(arr[i]);
+                        ThreadCommunication.print(anArr);
                         share.value = 1;
                         condition.signal();
                     } catch (Exception e) {
@@ -201,7 +212,7 @@ class MethodThree {
         int value = 1;
     }
 
-    public Runnable threadOne() {
+    Runnable threadOne() {
         final String[] inputArr = ThreadCommunication.buildNoArr(52);
         return new Runnable() {
             private String[] arr = inputArr;
@@ -217,16 +228,16 @@ class MethodThree {
         };
     }
 
-    public Runnable threadTwo() {
+    Runnable threadTwo() {
         final String[] inputArr = ThreadCommunication.buildCharArr(26);
         return new Runnable() {
             private String[] arr = inputArr;
 
             public void run() {
-                for (int i = 0; i < arr.length; i++) {
+                for (String anArr : arr) {
                     while (share.value == 1) {
                     }
-                    ThreadCommunication.print(arr[i]);
+                    ThreadCommunication.print(anArr);
                     share.value = 1;
                 }
             }
@@ -235,12 +246,12 @@ class MethodThree {
 }
 
 /**
- * 利用AtomicInteger
+ * 利用AtomicInteger（CAS利用处理器执行指令）
  */
 class MethodFour {
     private AtomicInteger share = new AtomicInteger(1);
 
-    public Runnable threadOne() {
+    Runnable threadOne() {
         final String[] inputArr = ThreadCommunication.buildNoArr(52);
         return new Runnable() {
             private String[] arr = inputArr;
@@ -256,17 +267,58 @@ class MethodFour {
         };
     }
 
-    public Runnable threadTwo() {
+    Runnable threadTwo() {
         final String[] inputArr = ThreadCommunication.buildCharArr(26);
         return new Runnable() {
             private String[] arr = inputArr;
 
             public void run() {
-                for (int i = 0; i < arr.length; i++) {
+                for (String anArr : arr) {
                     while (share.get() == 1) {
                     }
-                    ThreadCommunication.print(arr[i]);
+                    ThreadCommunication.print(anArr);
                     share.set(1);
+                }
+            }
+        };
+    }
+}
+
+/**
+ * 利用Queue
+ */
+class MethodFive {
+    private final Queue<String> queue = new ArrayBlockingQueue<>(2);
+
+    Runnable threadOne() {
+        final String[] inputArr = ThreadCommunication.buildNoArr(52);
+        return new Runnable() {
+            private String[] arr = inputArr;
+
+            public void run() {
+                for (int i = 0; i < arr.length; i = i + 2) {
+                    ThreadCommunication.print(arr[i], arr[i + 1]);
+                    queue.offer("char");
+                    while (!"no".equals(queue.peek())) {
+                    }
+                    queue.poll();
+                }
+            }
+        };
+    }
+
+    Runnable threadTwo() {
+        final String[] inputArr = ThreadCommunication.buildCharArr(26);
+        return new Runnable() {
+            private String[] arr = inputArr;
+
+            public void run() {
+                for (String anArr : arr) {
+                    while (!"char".equals(queue.peek())) {
+                    }
+                    ThreadCommunication.print(anArr);
+                    queue.poll();
+                    queue.offer("no");
                 }
             }
         };
